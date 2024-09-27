1c72ed90e58d4b44be505662e3e738cad4dc952f
hopemower <hopemower@mail.weber.edu>
2024-09-27 21:26:55 +0000

    Update assignment 04

---
+++ assignments/04/templates/nav.php
@@ -0,0 +1,50 @@
+    <!-- BEGIN PAGE HEADER -->
+    <header class="container">
+
+        <!-- BEGIN MAIN NAV -->
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="#">
+                    <span class="icon-text">
+                        <span class="icon"></span>
+                        <i class="fas fa-yin-yang fa-lg"></i>
+                        <span>&nbsp;Balance</span>
+                    </span>
+                </a>
+                <a class="navbar-burger" role="button" aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" href="#">Home</a>
+                    <a class="navbar-item" href="#">About</a>
+                </div>
+                <div>
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <a class="button is-light">Contact us</a>
+                            <a class="button is-primary">Log in</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- END MAIN NAV -->
+        <section class="block">&nbsp;<!--only for spacing purposes--></section>
+        <!-- BEGIN HERO -->
+        <section class="hero is-info">
+            <div class="hero-body">
+                <p class="title">Hero title</p>
+                <p class="subtitle">Hero subtitle</p>
+            </div>
+        </section>
+        <!-- END HERO -->
+
+    </header>
+    <!-- END PAGE HEADER -->
+
+    <!-- BEGIN MAIN PAGE CONTENT -->
+    <main class="container">
